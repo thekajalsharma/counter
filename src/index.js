1f8ec9d34093576e164abf57793589297f05e40d
unknown <thekajalsharma007@gmail.com>
2018-04-02 22:43:44 +0530

    Added Redux Store, dispatch and subscribe methods in index.js for Counter Component.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App1 from './App1';
 
-ReactDOM.render(<App1 />, document.getElementById('root'));
 
+//ReactDOM.render(<App3 />, document.getElementById('root'));
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+const store = createStore(reducer);
+
+
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+        <div>
+            <h1>{value}</h1>
+            <button onClick={onIncrement}>+</button>
+            <button onClick={onDecrement}>-</button>
+        </div>
+    );
+
+const render = () => {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() =>
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }
+            onDecrement={() =>
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }
+        />,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(render);
+render();
 
